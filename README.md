f054a728ea7ee35c74a4d63839d664d2909f387d
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-02-14 12:12:24 -0700

    added simple form validiation and centered main div

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# library
+# Library
+
+Library application for the Odin Project
+
+Assignment:
+
+## Features
+
+- New book button displays book input form
+
+- Form creates book objects that are then stored in an array and then a loop displays all books on the page
+
+- Remove button on each book as well as ability to change from 'read' or 'not read'
+
+### Process
+
+- Created book logic using object construcors
+
+- Created dynamic ul that updates the page as titles are added
+
+- Used preventDefault() on the submit button and took formData to create object props
+
+- Added logic to allow remove button to delete book and remove it from the DOM as well as the array
+
+- Added bookRead() which toggles a book to 'read' or 'not read'
+
+- Refactored using Class instead of object constructors
+
+- Centered layout and added logic to prevent leaving form blank
--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;        
-    };
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-    get info() {
-        //let readStatus = () => this.read === "false" ? "not read yet" : "read"; 
-        return `${this.title} by ${this.author}, ${this.pages} pages`;//, ${readStatus()}`;
-    };
-};
+  get info() {
+    //let readStatus = () => this.read === "false" ? "not read yet" : "read";
+    return `${this.title} by ${this.author}, ${this.pages} pages`; //, ${readStatus()}`;
+  }
+}
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
-};
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
 // const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "true");
 // const bloodMeridian = new Book("Blood Meridian", "Cormac McCarthy", 368, "false");
@@ -26,67 +26,78 @@ function addBookToLibrary(book){
 
 // console.table(myLibrary);
 
-const library = document.querySelector('.library');
-const ul = document.querySelector('.ul');
-let books = myLibrary.map(book => book.info);
+const library = document.querySelector(".library");
+const ul = document.querySelector(".ul");
+let books = myLibrary.map((book) => book.info);
 // console.log(books);
 for (let i = 0; i < books.length; i++) {
-    let book = books[i];
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(book));
-    ul.appendChild(li);
+  let book = books[i];
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(book));
+  ul.appendChild(li);
 }
 
-const bookForm = document.querySelector('.book-form');
-const toggle = () => (bookForm.style.display === 'none' ? bookForm.style.display = 'block' : bookForm.style.display = 'none');
-const newBook = document.querySelector('.new-book');
-newBook.addEventListener('click', toggle, false);
-const button = document.querySelector('.button');
+const bookForm = document.querySelector(".book-form");
+const toggle = () =>
+  bookForm.style.display === "none"
+    ? (bookForm.style.display = "block")
+    : (bookForm.style.display = "none");
+const newBook = document.querySelector(".new-book");
+newBook.addEventListener("click", toggle, false);
+const button = document.querySelector(".button");
 
-const submit = function(event){
-    const data = new FormData(bookForm);
-    const book = new Book;
-    for (const entry of data) {
-      book[entry[0]] = entry[1];
-    }
-    addBookToLibrary(book);
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(book.info));
-    let remove_btn = document.createElement('button');
-    remove_btn.textContent = 'Remove';
-    remove_btn.dataset.remove = myLibrary.length - 1;
-    let readBtn = document.createElement('button');
-    readBtn.textContent = bookRead(book);
-    readBtn.dataset.read = myLibrary.length - 1;
-    li.appendChild(readBtn);
-    li.appendChild(remove_btn);
-    ul.appendChild(li);
-    toggle();
-    event.preventDefault();
+const submit = function (event) {
+  if (bookForm.checkValidity() === false) {
+    bookForm.reportValidity();
+    return;
+  }
+  const data = new FormData(bookForm);
+  const book = new Book();
+  for (const entry of data) {
+    book[entry[0]] = entry[1];
+  }
+  addBookToLibrary(book);
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(book.info));
+  let remove_btn = document.createElement("button");
+  remove_btn.textContent = "Remove";
+  remove_btn.dataset.remove = myLibrary.length - 1;
+  let readBtn = document.createElement("button");
+  readBtn.textContent = bookRead(book);
+  readBtn.dataset.read = myLibrary.length - 1;
+  li.appendChild(readBtn);
+  li.appendChild(remove_btn);
+  ul.appendChild(li);
+  toggle();
+  event.preventDefault();
 };
-button.addEventListener('click', submit, false);
+button.addEventListener("click", submit, false);
 
 //add functionality to remove button
-ul.addEventListener('click', (e) => {
-    // check if read button
-    if ((Object.keys(e.target.dataset)[0]) === 'read'){
-        // console.log(e.target);
-        toggleRead(e.target);
-    } else if ((Object.keys(e.target.dataset)[0]) === 'remove') {
-        // the remove button
-        let confirm = window.confirm('Are you sure?');
-        if (confirm === true){
-        e.target.parentNode.remove();
-        myLibrary.splice(e.target.dataset.id, 1);
-        }
+ul.addEventListener("click", (e) => {
+  // check if read button
+  if (Object.keys(e.target.dataset)[0] === "read") {
+    // console.log(e.target);
+    toggleRead(e.target);
+  } else if (Object.keys(e.target.dataset)[0] === "remove") {
+    // the remove button
+    let confirm = window.confirm("Are you sure?");
+    if (confirm === true) {
+      e.target.parentNode.remove();
+      myLibrary.splice(e.target.dataset.id, 1);
     }
+  }
 });
 
-function bookRead(book){
-    return book.read === 'false' ? 'Not read yet' : 'Read';
+function bookRead(book) {
+  return book.read === "false" ? "Not read yet" : "Read";
 }
 
-function toggleRead(button){
-    button.textContent === 'Read' ? button.textContent = 'Not Yet Read' : button.textContent = 'Read';
-    myLibrary[button.dataset.read].read === 'true' ? myLibrary[button.dataset.read].read = 'false' : myLibrary[button.dataset.read].read = 'true';
+function toggleRead(button) {
+  button.textContent === "Read"
+    ? (button.textContent = "Not Yet Read")
+    : (button.textContent = "Read");
+  myLibrary[button.dataset.read].read === "true"
+    ? (myLibrary[button.dataset.read].read = "false")
+    : (myLibrary[button.dataset.read].read = "true");
 }
